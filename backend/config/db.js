8b819ec38897c9d424fb6c7e861993a90ec2241c
modsuhail25 <90952903+modsuhail25@users.noreply.github.com>
2025-01-08 13:26:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://modsuhail25:12345@cluster0.62xzl.mongodb.net/data"
+      ""
     );
     console.log(`Mongodb Connected:${conn.connection.host}`);
   } catch (error) {
