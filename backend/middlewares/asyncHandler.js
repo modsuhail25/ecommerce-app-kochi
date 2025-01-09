b2526f3baad35f9babd2acae967e3c5dd39e27d1
modsuhail25 <modsuhail25@gmail.com>
2025-01-09 21:35:26 +0530

    AsyncHandler - Bug-Fix

--- backend/middlewares/asyncHandler.js
+++ backend/middlewares/asyncHandler.js
@@ -1,11 +1,12 @@
-function asyncHandler(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next))
-      .then(() => {
-        next();
-      })
-      .catch(next);
-  };
-}
+// function asyncHandler(fn) {
+//   return function (req, res, next) {
+//     Promise.resolve(fn(req, res, next)).catch(next);
+//   };
+// }
+
+// No need of then in this promise because ,we are passing the function inside resolved(),if there is an error we need to pass it to next middleware that errorMiddleware
+
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 export default asyncHandler;
